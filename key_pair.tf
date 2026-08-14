8468efd109d578b81f3799c7cddd4e481c30b95b
sydaquib791 <syedakhuibhassan@gmail.com>
2026-08-14 20:54:26 +0530

    Fix SSH key generation in key_pair.tf

--- key_pair.tf
+++ key_pair.tf
@@ -1,4 +1,9 @@
+resource "tls_private_key" "rsa" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
 resource "aws_key_pair" "student_key" {
   key_name   = "student_key"
-  public_key = file("~/.ssh/id_rsa.pub")
+  public_key = tls_private_key.rsa.public_key_openssh
 }
