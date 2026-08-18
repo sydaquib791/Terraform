f8e28fa3bef286fc2c2762c12b38c7eb1d85cdad
sydaquib791 <syedakhuibhassan@gmail.com>
2026-08-18 18:40:20 +0530

    Delete backened.tf

--- backened.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket       = "terraform-bucket-7890"
-    key          = "dev/terraform.tfstate"
-    region       = "ap-south-1"
-    use_lockfile = true
-  }
-}
